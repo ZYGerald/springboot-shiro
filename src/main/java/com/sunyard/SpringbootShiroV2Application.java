6282ef846065629b917ce8d826129946f41fb991
Gerald <1032917944@qq.com>
2019-07-01 15:40:47 +0800

    create

---
+++ src/main/java/com/sunyard/SpringbootShiroV2Application.java
@@ -0,0 +1,13 @@
+package com.sunyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShiroV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShiroV2Application.class, args);
+    }
+
+}
